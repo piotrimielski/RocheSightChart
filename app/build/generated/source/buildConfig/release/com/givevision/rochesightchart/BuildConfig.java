8e1097f7c0e0e0be30cc27e5f5754a8d6c41d08c
piotrimielski <44473526+piotrimielski@users.noreply.github.com>
2020-08-20 20:44:32 +0100

    var 303 step 1: position of circles in horizontal bit further, allow the circles to move up or down a bit, add the maximum deviation possible from the original position, step2: to start  the circle a few sizes higher (on the middle of charts), test: a high pitch beep if right and low pitch beep if wrong response, if the user gets two wrong , it would be impossible for them to get 3/4 so you can stop the test there, make delay on key detection

--- app/build/generated/source/buildConfig/release/com/givevision/rochesightchart/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/givevision/rochesightchart/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.givevision.rochesightchart";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 302;
-  public static final String VERSION_NAME = "302";
+  public static final int VERSION_CODE = 303;
+  public static final String VERSION_NAME = "303";
 }
